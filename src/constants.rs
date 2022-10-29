c2961bdb479239f8dcdf74661d5bfe74c2f4ce9b
Fredpwol <fredpwol@gmail.com>
2022-10-29 11:13:40 +0100

    constant file

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,16 @@
-const BASE_URL: &str = "http://jiofi.local.html/";
+use std::path::PathBuf;
+
+use dirs::home_dir;
+
+pub const BASE_URL: &str = "http://jiofi.local.html/";
+pub const DB_PATH: &str = ".jaac/db/app.db";
+
+
+pub const SECRET_KEY: &str = "A@FD81##0$p0";
+
+
+
+pub fn get_db_full_path() -> PathBuf {
+    let path = home_dir().unwrap().join(DB_PATH);
+    path
+}
