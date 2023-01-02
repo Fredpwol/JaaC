b7935f6a8efdd797bd91282f45fd6c6439703b53
Fredpwol <fredpwol@gmail.com>
2023-01-02 23:01:03 +0100

    added new queries

--- src/queries.rs
+++ src/queries.rs
@@ -13,13 +13,25 @@ pub const CREATE_USER_TABLE: &str = "CREATE TABLE IF NOT EXISTS
                                             REFERENCES groups(id)
                                     )";
 
-pub const GET_ALL_USERS: &str = "SELECT host_name, ip_address, mac_address, status from users";
+pub const GET_ALL_USERS: &str = "SELECT id host_name, ip_address, mac_address, status from users";
 pub const GET_USER_BY_IP: &str =
-    "SELECT host_name, ip_address, mac_address, status from users where ip_address = ?";
+    "SELECT id host_name, ip_address, mac_address, status from users where ip_address = ? LIMIT 1";
 
-pub const CREATE_GROUP_TABLE: &str = "CREATE TABLE IF NOT EXISTS groups (id INTEGER PRIMARY KEY, name TEXT NOT NULL, mode TEXT DEFAULT '0')";
+pub const GET_USER_BY_HOST_NAME: &str =
+    "SELECT id host_name, ip_address, mac_address, status from users where host_name = ? LIMIT 1";
+pub const GET_USER_BY_MAC: &str =
+    "SELECT id host_name, ip_address, mac_address, status from users where mac_address = ? LIMIT 1";
+pub const GET_USER_BY_ID: &str =
+    "SELECT id host_name, ip_address, mac_address, status from users where id = ? LIMIT 1";
+pub const UPDATE_USER: &str =
+    "UPDATE users SET host_name=?1, ip_address=?2, mac_address?3, status=?4 WHERE id = ?5";
+
+pub const CREATE_GROUP_TABLE: &str = "CREATE TABLE IF NOT EXISTS groups (id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE , mode TEXT DEFAULT '0')";
 pub const SET_USER_GROUP: &str = "UPDATE users SET group_id=? WHERE id=?";
 
 pub const CREATE_DEFAULT_BLOCK_LIST: &str = "INSERT INTO groups(name) VALUES (default_block_list)";
-pub const CREATE_GROUP: &str = "INSERT INTO groups(name) VALUES (?1)";
+pub const CREATE_GROUP: &str = "INSERT INTO groups(name, mode) VALUES (?1, ?2)";
 
+pub const UPDATE_GROUP: &str = "UPDATE groups SET name=?1, mode=?2 WHERE name=?3";
+pub const SELECT_GROUP: &str = "SELECT id, name, mode FROM groups WHERE name = ?";
+pub const DELETE_GROUP: &str = "DELETE FROM groups WHERE name=?";
