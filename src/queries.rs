e5b872ea3bd2c56ac310fe5a1e63ee69ec5a7c2f
Fredpwol <fredpwol@gmail.com>
2023-01-03 14:47:29 +0100

    add more fields to user

--- src/queries.rs
+++ src/queries.rs
@@ -5,13 +5,17 @@ pub const CREATE_SESSION_RECORD: &str =
 pub const CREATE_USER_TABLE: &str = "CREATE TABLE IF NOT EXISTS
                                     users (
                                         id INTEGER PRIMARY KEY, host_name TEXT NOT NULL,
+                                        host_name TEXT NOT NULL, mac_address TEXT NOT NULL,
                                         ip_address TEXT NOT NULL, mac_address TEXT NOT NULL,
+                                        mac_address TEXT NOT NULL, mac_address TEXT NOT NULL,
                                         status TEXT DEFAULT 'Offline', group INTEGER FOREIGN
                                         group_id INTEGER
                                         CONSTRAINT fk_groups
                                             FOREIGN KEY (group_id)
                                             REFERENCES groups(id)
                                     )";
+
+pub const CREATE_USER_RECORD : &str= "INSERT INTO users (host_name, ip_address, mac_address, status) VALUES (? , ?, ?, ?)";
 
 pub const GET_ALL_USERS: &str = "SELECT id host_name, ip_address, mac_address, status from users";
 pub const GET_USER_BY_IP: &str =
@@ -24,7 +28,7 @@ pub const GET_USER_BY_MAC: &str =
 pub const GET_USER_BY_ID: &str =
     "SELECT id host_name, ip_address, mac_address, status from users where id = ? LIMIT 1";
 pub const UPDATE_USER: &str =
-    "UPDATE users SET host_name=?1, ip_address=?2, mac_address?3, status=?4 WHERE id = ?5";
+    "UPDATE users SET host_name=?1, ip_address=?2, mac_address=?3, status=?4 WHERE id = ?5";
 
 pub const CREATE_GROUP_TABLE: &str = "CREATE TABLE IF NOT EXISTS groups (id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE , mode TEXT DEFAULT '0')";
 pub const SET_USER_GROUP: &str = "UPDATE users SET group_id=? WHERE id=?";
@@ -33,5 +37,5 @@ pub const CREATE_DEFAULT_BLOCK_LIST: &str = "INSERT INTO groups(name) VALUES (de
 pub const CREATE_GROUP: &str = "INSERT INTO groups(name, mode) VALUES (?1, ?2)";
 
 pub const UPDATE_GROUP: &str = "UPDATE groups SET name=?1, mode=?2 WHERE name=?3";
-pub const SELECT_GROUP: &str = "SELECT id, name, mode FROM groups WHERE name = ?";
+pub const SELECT_GROUP: &str = "SELECT id, name, mode FROM groups WHERE name = ? LIMIT 1";
 pub const DELETE_GROUP: &str = "DELETE FROM groups WHERE name=?";
