0cc777a92065acb0686ad605f5d5f3e5037a6000
Fredpwol <fredpwol@gmail.com>
2023-01-03 14:47:57 +0100

    user command handler

--- src/bin/jaac.rs
+++ src/bin/jaac.rs
@@ -3,20 +3,19 @@ use std::collections::HashMap;
 use std::{io::Write, rc::Rc};
 
 use clap::Parser;
-use jaac::db::User;
+use jaac::db::{Group, User, UserIdentifier};
 #[allow(unused_variables, unused_imports)]
 use jaac::navigator;
 use jaac::navigator::ConnectedDevice;
 use jaac::types::Error;
 use jaac::utils::*;
 use jaac::{
-    cli::{Cli, Commands},
+    cli::{Cli, Commands, GroupSubCommand},
     db::SessionInfo,
 };
 use rpassword::read_password;
 use rusqlite::Connection;
 use tokio;
-
 fn get_jiofi_navigator(connection: Rc<Connection>) -> Result<navigator::JioPageNavigator, Error> {
     let session = SessionInfo::retrieve(Some(connection));
 
@@ -26,11 +25,6 @@ fn get_jiofi_navigator(connection: Rc<Connection>) -> Result<navigator::JioPageN
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    //         .expect("An Error Occured when trying to initialize client");
-    // // let conf = mynav.get_mac_config_page_data().await?;
-    // // let data = mynav.post_mac_config_page_data(conf).await?;
-    // // print!("{:?}", data);
-    // mynav.get_connected_devices().await?;
     let cli = Cli::parse();
 
     if let Some(args) = cli.args {
@@ -151,22 +145,89 @@ async fn main() -> Result<(), Error> {
             let (_, conn) = SessionInfo::new("".to_string(), "".to_string()).unwrap();
             let connection = Rc::new(conn);
             match command {
-                Commands::User(user_args) => (),
+                Commands::User(user_args) => {
+                    let mut nav = get_jiofi_navigator(Rc::clone(&connection))?;
+                    let mut identifier: Option<UserIdentifier> = None;
+                    if let Some(host_name) = &user_args.devicename {
+                        identifier = Some(UserIdentifier::HostName(host_name.to_owned()));
+                    } else if let Some(ip_address) = &user_args.ip {
+                        identifier = Some(UserIdentifier::IpAddress(ip_address.to_owned()));
+                    } else if let Some(mac_address) = &user_args.mac {
+                        identifier = Some(UserIdentifier::MacAddress(mac_address.to_owned()));
+                    }
+                    let user_identifier = identifier.expect("Please input a valid identfier, either the ip, mac, device name of the user.");
+
+                    let user =
+                        User::retrieve(user_identifier, Some(connection)).unwrap_or_else(|| {
+                            use tokio::runtime::Runtime;
+                            let connected_devices = Runtime::new()
+                                .unwrap()
+                                .block_on(nav.get_connected_devices())
+                                .expect("An error occured while retrieving connected devices");
+                            let mut filted_devices: Vec<ConnectedDevice> = connected_devices
+                                .into_iter()
+                                .filter(|device| {
+                                    let res = match &user_identifier {
+                                        UserIdentifier::HostName(host_name) => {
+                                            *host_name == *device.Host_name
+                                        }
+                                        UserIdentifier::IpAddress(ip_address) => {
+                                            *ip_address == *device.IP_address
+                                        }
+                                        UserIdentifier::MacAddress(mac_address) => {
+                                            *mac_address == *device.MAC
+                                        }
+                                        _ => panic!("Huh?"),
+                                    };
+                                    res
+                                })
+                                .collect();
+
+                            if filted_devices.len() == 0 {
+                                panic!("Sorry device was not found");
+                            }
+
+                            let device = filted_devices.pop().unwrap();
+
+                            let user = User::from(device);
+                            user
+                        });
+
+                    if user_args.block {}
+                }
                 Commands::Ls(ls_args) => {
                     if ls_args.connected {
                         let mut nav = get_jiofi_navigator(Rc::clone(&connection))?;
                         let connected_devices = nav.get_connected_devices().await?;
                         let mut user_list: Vec<User> = vec![];
 
-                        for device in &connected_devices {
+                        for device in connected_devices {
                             user_list.push(User::from(device))
                         }
 
                         User::print_user_list(user_list)?;
+                    } else if ls_args.all {
+                        let user_record = User::get_user_data(connection)?;
+                        let users = User::get_all_users(user_record);
+                        User::print_user_list(users)?;
+                    } else if ls_args.block_list {
+                    } else {
+                    }
+                }
+                Commands::Group(group_args) => {
+                    if let Some(sub_command) = &group_args.subcmd {
+                        match sub_command {
+                            GroupSubCommand::Create(group) => {
+                                let group = Group::new(&group.name, group.mode);
+                                group.sync(connection)?;
+                            }
+                            GroupSubCommand::Modify(group) => {}
+
+                            GroupSubCommand::Remove(group) => {}
+                        }
                     }
                 }
                 Commands::Thanos { snap } => (),
-                Commands::Group(group_args) => (),
             }
         }
         None => (),
